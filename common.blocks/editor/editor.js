c357699afe579c1d18bdeaaeaf6ad61859c76f90
sbmaxx <sbmaxx@yandex-team.ru>
2015-11-02 09:33:08 +0300

    dont show print margin

--- common.blocks/editor/editor.js
+++ common.blocks/editor/editor.js
@@ -11,6 +11,7 @@ modules.define('editor', ['i-bem__dom'], function(provide, BEMDOM) {
                     editor.setShowInvisibles(this.params.showInvisibles);
                     editor.setReadOnly(this.params.readOnly);
                     editor.setFontSize(this.params.fontSize);
+                    editor.setShowPrintMargin(false);
                     editor.$blockScrolling = Infinity;
                     editor.getSession().setMode(this.params.mode);
 
